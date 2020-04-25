886d08e70f101d7bd851f4e055a318e81c6b9d97
Dawid Maślankiewicz <maslankiewiczd@gmail.com>
2020-04-25 16:19:12 +0200

    Add scores table

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ var output = document.getElementById("text-out");
 var resultPlayer = document.getElementById('result-player');
 var resultComp = document.getElementById('result-comp');
 var gameNumber = document.getElementById('game-number');
+var gameTable = document.getElementById('game-table');
+
 
 var numberOfGames = 3;
 var playerWin = 0;
@@ -26,55 +28,57 @@ var draw = function () {
 }
 var names = function (number) {
     var out = '';
-    number == 1 ? out = 'Paper' : number == 2 ? out = 'Stone' : out = 'Scissors';
+    number == 1 ? out = 'paper' : number == 2 ? out = 'stone' : out = 'scissors';
     return out;
 }
 var log = function (text) {
     output.innerHTML = '<br>' + text + output.innerHTML;
 }
+var logTable = function(p,s,c){
+    gameTable.insertAdjacentHTML('afterbegin', '<tr><td>'+names(p)+'</td><td>'+s+'</td><td>'+names(c)+'</td></tr>');
+}
+
 var playerMove = function (player) {
 
     var comp = draw();
     console.log(comp);
 
     if ((player == 1 && comp == 2) || (player == 2 && comp == 1) || (player == 3 && comp == 2)) {
-        log('Win! You: ' + names(player) + '    Computer: ' + names(comp));
+        logTable(player, 'Win', comp);
         playerWin += 1;
 
     }
     else if (player == comp) {
-        log('It\'s DRAW... You: ' + names(player) + '    Computer: ' + names(comp));
-
+        logTable(player, 'Draw', comp);
+       
     }
     else {
-        log('LOSE :(( You type: ' + names(player) + '    Computer: ' + names(comp))
+        logTable(player, 'Lose', comp);
         compWin += 1;
     };
 
     if (playerWin >= numberOfGames) {
-        log('You WIN!! To play again, please click \'New Game\' button');
+        log('You WIN!! Click \'New Game\' button');
         canPlay = false;
 
     }
     else if (compWin >= numberOfGames) {
-        log('Comp WIN !! To play again, please click \'New Game\' button');
+        log('Comp WIN !! Click \'New Game\' button');
         canPlay = false;
     }
     resultPlayer.innerHTML = playerWin;
     resultComp.innerHTML = compWin;
-
-
 }
 
 boxPaper.addEventListener('click', function () {
-
-    canPlay ? playerMove(1) : log('Game over, please press the new game button!');
+    
+    canPlay ? playerMove(1) : log('Game over, press the new game button!');
 });
 boxStone.addEventListener('click', function () {
-    canPlay ? playerMove(2) : log('Game over, please press the new game button!');
+    canPlay ? playerMove(2) : log('Game over, press the new game button!');
 });
 boxScissors.addEventListener('click', function () {
-    canPlay ? playerMove(3) : log('Game over, please press the new game button!');
+    canPlay ? playerMove(3) : log('Game over, press the new game button!');
 });
 
 boxNew.addEventListener('click', function () {
@@ -86,6 +90,9 @@ boxNew.addEventListener('click', function () {
     numberOfGames = window.prompt('How many games do You want to play?');
     gameNumber.innerHTML = numberOfGames;
     canPlay = true;
+    gameTable.innerHTML = '';
+    resultComp.innerHTML = 0;
+    resultPlayer.innerHTML = 0;
 
 })
 

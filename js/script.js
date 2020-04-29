17fe1244c08aaa834339178f9d9ba7eeff3e71bb
Dawid Maślankiewicz <maslankiewiczd@gmail.com>
2020-04-29 23:35:15 +0200

    Add modal with score

--- js/script.js
+++ js/script.js
@@ -13,11 +13,15 @@ var resultComp = document.getElementById('result-comp');
 var gameNumber = document.getElementById('game-number');
 var gameTable = document.getElementById('game-table');
 
+var modals = document.querySelectorAll('.modal');
+var modalSection = document.querySelector('#modals-overlay');
+var closeButtoms = document.querySelectorAll('.modal .close');
+
 var params = {
     numberOfGames: 3,
     playerWin: 0,
     compWin: 0,
-    canPlay: true
+    canPlay: true,
 }
 
 
@@ -28,11 +32,13 @@ var draw = function () {
 }
 var names = function (number) {
     var out = '';
-    number === 1 ? out = 'paper' : number === 2 ? out = 'stone' : out = 'scissors';
+    number == 1 ? out = 'paper' : number == 2 ? out = 'stone' : out = 'scissors';
     return out;
 }
 var log = function (text) {
-    output.innerHTML = '<br>' + text;
+    output.innerHTML = '<br><br>' + text;
+    showModal();
+
 }
 var logOver = function () {
     outputOver.innerHTML = '<br> Game over, press the new game button!';
@@ -42,50 +48,68 @@ var logTable = function (p, s, c) {
     gameTable.insertAdjacentHTML('afterbegin', '<tr><td>' + names(p) + '</td><td>' + s + '</td><td>' + names(c) + '</td></tr>');
 }
 var isNumber = function (number) {
-    (isNaN(number) || !number || number <= 0) ? (log('Please insert number > 0'), params.canPlay = false) : (log(''), params.canPlay = true);
+    (isNaN(number) || !number || number <= 0) ? (log('Please insert number > 0'), params.canPlay = false) : ( params.canPlay = true);
+}
+var showModal = function () {
+    for (var i = 0; i < modals.length; i++) {
+        modals[i].classList.remove('show');
+    }
+    modalSection.classList.add('show');
+    document.querySelector('#modal-score').classList.add('show');
 }
 
-var playGame = function (player) {
+var hideModal = function () {
+    modalSection.classList.remove('show');
+}
+
+
+var playGame = function (event) {
+
+    var player = event.target.getAttribute('data-move');
 
     var comp = draw();
 
-    if ((player === 1 && comp === 2) || (player === 2 && comp === 3) || (player === 3 && comp == 1)) {
+    if ((player == 1 && comp == 2) || (player == 2 && comp == 3) || (player == 3 && comp == 1)) {
         logTable(player, 'Win', comp);
         params.playerWin++;
 
     }
-    else if (player === comp) {
+    else if (player == comp) {
         logTable(player, 'Draw', comp);
 
     }
     else {
         logTable(player, 'Lose', comp);
         params.compWin++;
+
     };
 
     if (params.playerWin >= params.numberOfGames) {
-        log('You WIN!! Click \'New Game\' button');
         params.canPlay = false;
-
+        log('You WIN!! Click \'New Game\' button');
+       
     }
     else if (params.compWin >= params.numberOfGames) {
-        log('Comp WIN !! Click \'New Game\' button');
         params.canPlay = false;
+        log('Comp WIN !! Click \'New Game\' button');
+      
     }
     resultPlayer.innerHTML = params.playerWin;
     resultComp.innerHTML = params.compWin;
 }
 
 for (var i = 0; i < playerMove.length; i++) {
-    var temp = playerMove[i].getAttribute('data-move');
-    playerMove[i].addEventListener('click', function () {
-        params.canPlay ? playGame(temp) : logOver();
-    })
+        playerMove[i].addEventListener('click', function(event){
+            params.canPlay ? playGame(event) : log('<br> Game over, press the new game button!')
+        })
 }
 
+for (var i = 0; i < closeButtoms.length; i++) {
+    closeButtoms[i].addEventListener('click', hideModal);
+}
 boxNew.addEventListener('click', function () {
-    output.innerHTML = '';
-    outputOver.innerHTML = '';
+  //  output.innerHTML = '';
+   // outputOver.innerHTML = '';
     resultPlayer.innerHTML = '';
     resultComp.innerHTML = '';
     gameTable.innerHTML = '';
